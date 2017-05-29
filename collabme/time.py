4e800605d65266c959c28f5612081593004669bd
Joseph H Kennedy <kennedyjh@ornl.gov>
2017-05-28 20:09:40 -0400

    Update method for filtering entries that contain your last name
    Slightly less complicated than setting a flag, but more loopy.
    
    Also, clarifies it's using your last name, not full name, and a warning
    for common last name.
    
    Ensures that the last name filter option is option ( #6 )

---
+++ collabme/time.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+current_year = date.today().year  
+
+
+def clean_year(year_string):
+    try:
+        year = int(year_string[:4])  # Years are only 4 char; will catch YYYYa, YYYYb, etc.
+    except ValueError as E:
+        alts = ['prep', 'press', 'review', 'accepted']
+        for alt in alts:
+            if alt in year_string.lower():
+                year = current_year
+                break
+        else:
+            # alts loopfell through without finding a suitable alt
+            raise E
+
+    return year
